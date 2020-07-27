b832f166ea2bad109151eef4465eac869f105be2
linoy18 <46220147+linoy18@users.noreply.github.com>
2020-07-28 00:04:07 +0300

    phones finish

--- client.js
+++ client.js
@@ -385,18 +385,32 @@ function getPhones() {
 
                 for (var j = 0; j < obj.models.length; j++) { //foreach type model (size) 
                     var objModel = obj.models[j];
-                    innerTypes += `<div class="size" onclick="showPriceAndText('${objModel.price}','${objModel.text}','${i}','${objModel.type}')">` + objModel.type + `</div>`;
+                    innerTypes += `<Button class="size" onclick="showPriceAndText('${objModel.price}','${objModel.text}','${i}','${objModel.type}')">` + objModel.type + `</Button>`;
                 } //end inserting type models
 
                 var dataRow = `<div class="container1">
-                <div class="images"><img class="img_product" src=`+ phoneImg + `/></div> 
+                <div class="row">
+                <div class="col-6">
+                <img class="img_product" src=`+ phoneImg + `/>
+                </div>
+                <div class="col-6"> 
+                <div class="product"> 
+                <p>Phone for sale</p><h1>`+ obj.id + `</h1>
+                <div id="price-${i}" class="price1"></div>
+                <p class="desc">`+ obj.description + `</p>
+                <div id="text-${i}"></div> 
+                </div> 
+                </div>  
+                </div>
+                <div class="row">
+                <div class="col-6">
                 <p class="pick">Choose Memory Size</p>
                 <div class="sizes">`+ innerTypes + `</div>
-                <div class="product"> <p>Phone for sale</p><h1>`+ obj.id + `</h1><div id="price-${i}" class="price1"></div>
-                  <p class="desc">`+ obj.description + `</p>
-                  <div id="text-${i}"></div>
-                  <div class="buttons"><button id="addToCart-${i}" class="add" onclick="addToCart('${obj.id}','${i}')">Add to Cart</button></div>
-                </div></div>`;
+                </div>
+                <div class="col-6">
+                <div class="buttons"><button id="addToCart-${i}" class="add" onclick="addToCart('${obj.id}','${i}')">Add to Cart</button></div>
+                </div></div>
+                </div> `;
                 $(dataRow).appendTo('#wrapper1');
             } //end inserting all phones
         },
